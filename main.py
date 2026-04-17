519a0f66ff81e2479afdfd8ee9da60a02fcc788f
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-04-17 18:41:40 +0200

    use overlay over icon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from app.tray import run_tray
+# from app.tray import run_tray
+from app.overlay import run_overlay
 
 if __name__ == "__main__":
-    run_tray()
+    # run_tray()
+    run_overlay()
