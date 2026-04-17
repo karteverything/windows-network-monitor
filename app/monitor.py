3bf2b18ceca8e51966cd8a2ce2ec19d4fa2ae4f9
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-04-17 18:10:38 +0200

    define attr

--- app/monitor.py
+++ app/monitor.py
@@ -1,16 +1,19 @@
 import psutil
-import time
 
 class NetWorkMonitor:
     def __init__(self):
-        self.last = psutil.net_io_counters()
+        net = psutil.net_io_counters()
+        self.bytes_sent = net.bytes_sent
+        self.bytes_recv = net.bytes_recv
 
     def get_speed(self, interval = 1):
-        time.sleep(interval)
         current = psutil.net_io_counters()
 
         upload = (current.bytes_sent - self.bytes_sent) / 1024
         download = (current.bytes_recv - self.bytes_recv) / 1024
 
-        self.last = current
-        return int(upload), int(download)
+        # update stored values
+        self.bytes_sent = current.bytes_sent
+        self.bytes_recv = current.bytes_recv
+
+        return round(upload, 2), round(download, 2)
