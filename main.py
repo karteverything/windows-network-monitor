7e961d6789c1c73934747f28c0315230ccfb15dc
Mlungisi <sibitanemlungisi26@gmail.com>
2026-07-31 09:19:09 +0200

    thread tray and overlay

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# from app.tray import run_tray
+from app.tray import run_tray
 from app.overlay import run_overlay
+import threading
 
 if __name__ == "__main__":
-    # run_tray()
+    tray_thread = threading.Thread(
+        target=run_tray,
+        daemon=True
+    )
+    tray_thread.start()
     run_overlay()
