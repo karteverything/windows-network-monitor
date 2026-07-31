5aae4c6c41e9f06b994f3f8ea9adc1883570d143
Mlungisi <sibitanemlungisi26@gmail.com>
2026-07-31 09:33:32 +0200

    keep tkinter on main thread and tray on the bg

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from app.overlay import run_overlay
 import threading
 
 if __name__ == "__main__":
-    tray_thread = threading.Thread(
+    threading.Thread(
         target=run_tray,
         daemon=True
-    )
-    tray_thread.start()
+    ).start()
+    
     run_overlay()
